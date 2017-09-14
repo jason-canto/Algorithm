89018abafbd89309cb4dce94b6e46e6abe0f5283
jason.canto <jason_canto@yahoo.com.br>
2017-09-13 23:11:14 -0300

    Triangular numbers algorithm

--- src/main/java/calc/Euler.java
+++ src/main/java/calc/Euler.java
@@ -1,10 +1,13 @@
 package calc;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Euler {
-	
+
 	public static int sumProblem1() {
 		int total = 0;
 		for (int i = 0; i < 1000; i++) {
@@ -179,7 +182,7 @@ public class Euler {
 		}
 		return max;
 	}
-	
+
 	public static long sumPrime(long n) {
 		long i = 2;
 		long nPrime = 0;
@@ -194,34 +197,33 @@ public class Euler {
 		}
 		return sum;
 	}
-	
+
 	public static long largestProducGrid() {
-		int matrix[][] = 
-			{{8, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 8},
-			{49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00},
-			{81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65},
-			{52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91},
-			{22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80},
-			{24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50},
-			{32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70},
-			{67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21},
-			{24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72},
-			{21, 36, 23, 9, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95},
-			{78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 9, 53, 56, 92},
-			{16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57},
-			{86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58},
-			{19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40},
-			{04, 52, 8, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66},
-			{88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69},
-			{04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36},
-			{20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16},
-			{20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54},
-			{01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48}};
-		
+		int matrix[][] = { { 8, 02, 22, 97, 38, 15, 00, 40, 00, 75, 04, 05, 07, 78, 52, 12, 50, 77, 91, 8 },
+				{ 49, 49, 99, 40, 17, 81, 18, 57, 60, 87, 17, 40, 98, 43, 69, 48, 04, 56, 62, 00 },
+				{ 81, 49, 31, 73, 55, 79, 14, 29, 93, 71, 40, 67, 53, 88, 30, 03, 49, 13, 36, 65 },
+				{ 52, 70, 95, 23, 04, 60, 11, 42, 69, 24, 68, 56, 01, 32, 56, 71, 37, 02, 36, 91 },
+				{ 22, 31, 16, 71, 51, 67, 63, 89, 41, 92, 36, 54, 22, 40, 40, 28, 66, 33, 13, 80 },
+				{ 24, 47, 32, 60, 99, 03, 45, 02, 44, 75, 33, 53, 78, 36, 84, 20, 35, 17, 12, 50 },
+				{ 32, 98, 81, 28, 64, 23, 67, 10, 26, 38, 40, 67, 59, 54, 70, 66, 18, 38, 64, 70 },
+				{ 67, 26, 20, 68, 02, 62, 12, 20, 95, 63, 94, 39, 63, 8, 40, 91, 66, 49, 94, 21 },
+				{ 24, 55, 58, 05, 66, 73, 99, 26, 97, 17, 78, 78, 96, 83, 14, 88, 34, 89, 63, 72 },
+				{ 21, 36, 23, 9, 75, 00, 76, 44, 20, 45, 35, 14, 00, 61, 33, 97, 34, 31, 33, 95 },
+				{ 78, 17, 53, 28, 22, 75, 31, 67, 15, 94, 03, 80, 04, 62, 16, 14, 9, 53, 56, 92 },
+				{ 16, 39, 05, 42, 96, 35, 31, 47, 55, 58, 88, 24, 00, 17, 54, 24, 36, 29, 85, 57 },
+				{ 86, 56, 00, 48, 35, 71, 89, 07, 05, 44, 44, 37, 44, 60, 21, 58, 51, 54, 17, 58 },
+				{ 19, 80, 81, 68, 05, 94, 47, 69, 28, 73, 92, 13, 86, 52, 17, 77, 04, 89, 55, 40 },
+				{ 04, 52, 8, 83, 97, 35, 99, 16, 07, 97, 57, 32, 16, 26, 26, 79, 33, 27, 98, 66 },
+				{ 88, 36, 68, 87, 57, 62, 20, 72, 03, 46, 33, 67, 46, 55, 12, 32, 63, 93, 53, 69 },
+				{ 04, 42, 16, 73, 38, 25, 39, 11, 24, 94, 72, 18, 8, 46, 29, 32, 40, 62, 76, 36 },
+				{ 20, 69, 36, 41, 72, 30, 23, 88, 34, 62, 99, 69, 82, 67, 59, 85, 74, 04, 36, 16 },
+				{ 20, 73, 35, 29, 78, 31, 90, 01, 74, 31, 49, 71, 48, 86, 81, 16, 23, 57, 05, 54 },
+				{ 01, 70, 54, 71, 83, 51, 54, 69, 16, 92, 33, 48, 61, 43, 52, 01, 89, 19, 67, 48 } };
+
 		long max = 0;
-		
-		for(int i=0; i < 20; i++) {
-			for(int j=0; j < 20; j++) {
+
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 20; j++) {
 				long up = 0;
 				long down = 0;
 				long left = 0;
@@ -230,47 +232,47 @@ public class Euler {
 				long northWest = 0;
 				long southEast = 0;
 				long southWest = 0;
-				if(i-3 >= 0) {
-					up = matrix[i][j] * matrix[i-1][j] * matrix[i-2][j] * matrix[i-3][j];
+				if (i - 3 >= 0) {
+					up = matrix[i][j] * matrix[i - 1][j] * matrix[i - 2][j] * matrix[i - 3][j];
 				}
-				if(i+3 < matrix.length) {					
-					down = matrix[i][j] * matrix[i+1][j] * matrix[i+2][j] * matrix[i+3][j];
+				if (i + 3 < matrix.length) {
+					down = matrix[i][j] * matrix[i + 1][j] * matrix[i + 2][j] * matrix[i + 3][j];
 				}
-				if(j-3 >= 0) {
-					left = matrix[i][j] * matrix[i][j-1] * matrix[i][j-2] * matrix[i][j-3];
+				if (j - 3 >= 0) {
+					left = matrix[i][j] * matrix[i][j - 1] * matrix[i][j - 2] * matrix[i][j - 3];
 				}
-				if(j+3 < matrix[j].length) {
-					right = matrix[i][j] * matrix[i][j+1] * matrix[i][j+2] * matrix[i][j+3];
+				if (j + 3 < matrix[j].length) {
+					right = matrix[i][j] * matrix[i][j + 1] * matrix[i][j + 2] * matrix[i][j + 3];
 				}
-				if(i-3 >= 0 && j-3 >= 0) {
-					northWest = matrix[i][j] * matrix[i-1][j-1] * matrix[i-2][j-2] * matrix[i-3][j-3];
+				if (i - 3 >= 0 && j - 3 >= 0) {
+					northWest = matrix[i][j] * matrix[i - 1][j - 1] * matrix[i - 2][j - 2] * matrix[i - 3][j - 3];
 				}
-				if(i-3 >= 0 && j+3 < matrix[j].length) {
-					northEast = matrix[i][j] * matrix[i-1][j+1] * matrix[i-2][j+2] * matrix[i-3][j+3];
+				if (i - 3 >= 0 && j + 3 < matrix[j].length) {
+					northEast = matrix[i][j] * matrix[i - 1][j + 1] * matrix[i - 2][j + 2] * matrix[i - 3][j + 3];
 				}
-				if(i+3 < matrix.length && j-3 >= 0) {
-					southEast = matrix[i][j] * matrix[i+1][j-1] * matrix[i+2][j-2] * matrix[i+3][j-3];
-					
+				if (i + 3 < matrix.length && j - 3 >= 0) {
+					southEast = matrix[i][j] * matrix[i + 1][j - 1] * matrix[i + 2][j - 2] * matrix[i + 3][j - 3];
+
 				}
-				if(i+3 < matrix.length && j+3 < matrix[j].length) {
-					southWest = matrix[i][j] * matrix[i+1][j+1] * matrix[i+2][j+2] * matrix[i+3][j+3];
+				if (i + 3 < matrix.length && j + 3 < matrix[j].length) {
+					southWest = matrix[i][j] * matrix[i + 1][j + 1] * matrix[i + 2][j + 2] * matrix[i + 3][j + 3];
 				}
-				if(up > max) {
+				if (up > max) {
 					max = up;
 				}
-				if(down > max) {
+				if (down > max) {
 					max = down;
 				}
-				if(right > max) {
+				if (right > max) {
 					max = right;
 				}
-				if(left > max) {
+				if (left > max) {
 					max = left;
 				}
-				if(northEast > max) {
+				if (northEast > max) {
 					max = northEast;
 				}
-				if(northWest > max) {
+				if (northWest > max) {
 					max = northWest;
 				}
 				if (southEast > max) {
@@ -284,9 +286,8 @@ public class Euler {
 		return max;
 	}
 
-	public static String sumBigNumber(){
-		String bigNumbers [] = {
-				"37107287533902102798797998220837590246510135740250",
+	public static String sumBigNumber() {
+		String bigNumbers[] = { "37107287533902102798797998220837590246510135740250",
 				"46376937677490009712648124896970078050417018260538",
 				"74324986199524741059474233309513058123726617309629",
 				"91942213363574161572522430563301811072406154908250",
@@ -385,13 +386,13 @@ public class Euler {
 				"77158542502016545090413245809786882778948721859617",
 				"72107838435069186155435662884062257473692284509516",
 				"20849603980134001723930671666823555245252804609722",
-				"53503534226472524250874054075591789781264330331690"};
-		
-		BigDecimal sum = BigDecimal.ZERO;	
-		for(int i=0; i < bigNumbers.length; i++){			
-			sum = sum.add(new BigDecimal(bigNumbers[i]));			
+				"53503534226472524250874054075591789781264330331690" };
+
+		BigDecimal sum = BigDecimal.ZERO;
+		for (int i = 0; i < bigNumbers.length; i++) {
+			sum = sum.add(new BigDecimal(bigNumbers[i]));
 		}
-		return sum.toPlainString().substring(0,10);
+		return sum.toPlainString().substring(0, 10);
 	}
 
 	public static long powerDigitSum(int x, int power) {
@@ -420,23 +421,69 @@ public class Euler {
 
 	public static long longestCollatzSequence() {
 		long max = 0;
-		for(int i=1; i < 1000000; i++){
+		for (int i = 1; i < 1000000; i++) {
 			int n = i;
 			long count = 0;
-			while(n != 1){
-				if(n % 2 == 0){
-					n = n/2;
+			while (n != 1) {
+				if (n % 2 == 0) {
+					n = n / 2;
 				} else {
 					n = 3 * n + 1;
 				}
 				System.out.println(count);
 				count++;
 			}
-			if(count > max) {
+			if (count > max) {
 				max = count;
 			}
 		}
 		return max;
+	}
+
+	public static long triangularNumbers() {
+		int countDivisors = 0;
+		long n = 0;
+		long i = 0;
+		while (countDivisors <= 500) {
+			n += i;
+			countDivisors = getTotalDivisors(n);
+			System.out.println(n + " = " + countDivisors);
+			i++;
+		}
+		return n;
+	}
+
+	public static int getTotalDivisors(long n) {
+		long limit = n;
+		int numberOfDivisors = 0;
+
+		if (n == 1)
+			return 1;
+
+		for (long i = 1; i < limit; i++) {
+			if (n % i == 0) {
+				limit = n / i;
+				if (limit != i) {
+					numberOfDivisors++;
+				}
+				numberOfDivisors++;
+			}
+		}
+
+		return numberOfDivisors;
+	}
+
+	public static List<Long> getDivisorsList(long n) {
+		List<Long> divisors = new ArrayList<Long>();
+		for(long i=1; i <= Math.sqrt(n); i++){
+			if(n % i == 0){
+				divisors.add(i);
+				if(i != Math.sqrt(n)){
+					divisors.add(n/i);
+				}
+			}
+		}
+		return divisors;
 	}
 
 }
--- src/main/java/calc/Main.java
+++ src/main/java/calc/Main.java
@@ -3,6 +3,6 @@ package calc;
 public class Main {
 
 	public static void main(String args[]) {
-		System.out.println(Euler.longestCollatzSequence());
+		System.out.println(Euler.triangularNumbers());
 	}
 }
